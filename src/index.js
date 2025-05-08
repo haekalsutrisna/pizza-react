132167717260611d3b6500974b054d93433072e7
Muhammad Haekal Sutrisna <78276395+haekalsutrisna@users.noreply.github.com>
2025-05-08 08:03:50 +0700

    update

--- src/index.js
+++ src/index.js
@@ -73,7 +73,7 @@ function Header() {
   const style = {};
   return (
     <header className="header">
-      <h1 style={style}>Fast Haekal Pizza Co.</h1>;
+      <h1 style={style}>Fast Pizza Co.</h1>;
     </header>
   );
 }
